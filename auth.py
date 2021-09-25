4ff2d3280c554a53d9370bd4af144f698e3c914a
Ivan <galochkinia@ugrafmsh.ru>
2021-09-25 11:45:05 +0000

    Update auth.py

--- auth.py
+++ auth.py
@@ -16,20 +16,7 @@ app = FastAPI()
 
 global_init()
 
-origins = [
-    "192.168.48.4",
-    "http://localhost:8080",
-    "https://earth-quiz.uk",
-    "https://api.earth-quiz.uk",
-    "http://earth-quiz.uk",
-    "http://api.earth-quiz.uk",
-    "https://127.0.0.1",
-    "http://127.0.0.1",
-    "www.api.earth-quiz.uk",
-    "api.earth-quiz.uk",
-    "http://192.168.48.4"
-    "https://192.168.48.4"
-]
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
